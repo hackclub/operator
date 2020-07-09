f64c664003e0f9a400fd57d742a7d04d86483060
digitaldina <dinaelhanan@gmail.com>
2020-07-09 19:14:57 +0000

    fix slack oauth2 call

--- pages/api/slack/authuser.js
+++ pages/api/slack/authuser.js
@@ -2,7 +2,7 @@ const _ = require('lodash')
 const { WebClient: SlackWebClient } = require('@slack/web-api')
 const AirtablePlus = require('airtable-plus')
 
-const slack = new SlackWebClient(process.env.SLACK_BOT_TOKEN)
+const slack = new SlackWebClient(process.env.SLACK_CLIENT_SECRET)
 const botSpamId = 'C0P5NE354'
 
 export default async (req, res) => {
@@ -23,7 +23,11 @@ export default async (req, res) => {
     state
   } = params
   
-  const result = await slack.oauth.v2.access({code})
+  const result = await slack.oauth.v2.access({
+    code,
+    'client_id': process.env.SLACK_CLIENT_ID,
+    'client_secret': process.env.SLACK_CLIENT_SECRET
+  })
   console.log('sent code:', code, 'to slack and got back', result)
   
   return res.json({ok: true})
